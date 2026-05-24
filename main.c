839827545e0d2bdb9d148372c2c91668926b3e2b
Rottschaefer <rottschaefer54@gmail.com>
2026-05-24 18:38:31 -0300

    Mais um resultado

--- main.c
+++ main.c
@@ -250,7 +250,7 @@ int main(int argc, char* argv[]){
         .quantization = usearch_scalar_f32_k, // or f32_k, bf16_k, e5m2_k, e4m3_k, e3m2_k, e2m3_k, i8_k, u8_k
         .dimensions = dimensions,
         .expansion_add = 0, // for defaults
-        .expansion_search = 32 // for defaults
+        .expansion_search = 128 // for defaults
     };
     global_index = usearch_init(&opts, &error);
 
